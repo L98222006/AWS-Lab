27b5671db2c0e9eedf7b0b6a69acffbf33bdc7d6
Yifu <m9822006@hotmail.com.tw>
2017-08-12 22:09:08 +0800

    lab4 create

---
+++ Lab4/index.php
@@ -0,0 +1,11 @@
+
+<?php
+echo '伺服器名稱：', $_SERVER['SERVER_NAME'], "<br/>";
+
+shell_exec( 'echo "<?php while(1) ; " | php > /dev/null 2>&1 & ' );
+
+@date_default_timezone_set("Asia/Taipei");
+$current_datetime = @date('Y-m-d H:i:s', @time());
+echo "私有 IP： @ $current_datetime：".file_get_contents('/etc/hostname')."\n";
+
+
